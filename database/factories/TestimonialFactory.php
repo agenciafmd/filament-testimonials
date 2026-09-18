49ba10703d728bc7150753f4a0ceeadf9a99e3d2
Irineu Junior <irineu@fmd.ag>
2026-09-18 15:50:59 -0300

    refactor(factories): simplifica lógica de cálculo de ratio

--- database/factories/TestimonialFactory.php
+++ database/factories/TestimonialFactory.php
@@ -21,11 +21,6 @@ final class TestimonialFactory extends Factory
         $slug = str($name)
             ->slug()
             ->toString();
-        $ratio = collect(config('filament-testimonials.image.ratio', ['4:3']))
-            ->map(fn (string $ratio) => str($ratio)
-                ->replace(':', 'x')
-                ->toString())
-            ->implode('');
 
         return [
             'is_active' => fake()->boolean(),
@@ -34,7 +29,7 @@ final class TestimonialFactory extends Factory
             'short_description' => config('filament-testimonials.short_description.visible') ? fake()->text() : null,
             'description' => config('filament-testimonials.description.visible') ? fake()->text(maxNbChars: 200) : null,
             'video' => config('filament-testimonials.video.visible') ? fake()->youtubeRandomUri() : null,
-            'image' => config('filament-testimonials.image.visible') ? Storage::putFile('fake', fake()->localImage(ratio: $ratio)) : null,
+            'image' => config('filament-testimonials.image.visible') ? Storage::putFile('fake', fake()->localImage(ratio: '4:3')) : null,
             'slug' => $slug,
         ];
     }
